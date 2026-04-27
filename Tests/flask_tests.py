da53a4df1c8281cb6c124ae4199bb7b9b24dfe43
bec-mor <becca.r.moran@gmail.com>
2026-04-27 18:14:16 -0500

    Final changes!

--- Tests/flask_tests.py
+++ Tests/flask_tests.py
@@ -17,28 +17,28 @@ class TestSOMETHING(unittest.TestCase):
         self.assertEqual(b'Hello, this is the homepage of LLM Energy Route Assignment', response.data) 
 
     def test_route_column(self): 
-        '''docstring comment'''
+        '''Tests whether a specified column returns the correct output'''
         self.app = app.test_client() 
         load_data()
         response = self.app.get('/0/model_parameters_billion/', follow_redirects=True) 
         self.assertEqual(b"['model_parameters_billion', '175', '1800', '540', '176', '671', '405', '2000', '70', '20', '550', 'Not disclosed', '11', '619', '1500', '0.55', '70', '1200', '180', '7.3', '47', '72', '34', '2700', '280', '175', '7', '7']", response.data) 
 
     def test_route_nonexistent_column(self):
-        '''docstring comment'''
+        '''Tests whether a nonexistent column returns an empty string'''
         self.app = app.test_client() 
         load_data()
         response = self.app.get('/0/not_real_column/', follow_redirects=True)
         self.assertEqual(b"[]\n", response.data) 
 
     def test_route_row(self): 
-        '''docstring comment'''
+        '''Tests whether a specified row returns the correct output'''
         self.app = app.test_client() 
         load_data()
         response = self.app.get('/T5/', follow_redirects=True) 
         self.assertEqual(b"['T5', '11', '500', 'TPUv3', '512', '480', 'US Central (Iowa)', '1.12', '450', '545', '123900', '46700']", response.data) 
 
     def test_route_nonexistent_row(self): 
-        '''docstring comment'''
+        '''Tests whether a nonexistent row returns the correct error message'''
         self.app = app.test_client() 
         load_data()
         response = self.app.get('/not_real_LLM_name/', follow_redirects=True) 
